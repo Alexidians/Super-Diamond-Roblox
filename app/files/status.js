6684da78186d940d35fbb54d39a171ff056dcb2e
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-23 23:06:43 +0300

    Update status.js

--- app/files/status.js
+++ app/files/status.js
@@ -1,5 +1,5 @@
 var Down = true
 if(Down) {
- document.body.remove()
+ alert("Sorry. Super Diamond Roblox is Down")
  location.replace("https://alexidians.github.io/Super-Diamond-Roblox/app/files/down.html")
 }
