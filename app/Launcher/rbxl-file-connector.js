4f55fb83a30fbcb9d90f30992922ab53a4ffd037
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-23 20:23:35 +0300

    Delete rbxl-file-connector.js

--- app/Launcher/rbxl-file-connector.js
+++
@@ -1,5 +0,0 @@
-var Rbxl = document.createElement("iframe")
-Rbxl.src = "https://alexidians.github.io/Super-Diamond-Roblox/app/files/roblox.html"
-Rbxl.width = "100%"
-Rbxl.height = "100%"
-document.body.appendChild(Rbxl)
