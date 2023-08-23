fe1c93745c92e4e1dbe79f398f3d57aa561f0f52
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-23 19:43:43 +0300

    Delete Launcher directory

--- Launcher/rbxl-file-connector.js
+++
@@ -1,5 +0,0 @@
-var Rbxl = document.createElement("iframe")
-Rbxl.src = "http://super-diamond-community.great-site.net/roblox/app/files/roblox.html"
-Rbxl.width = "100%"
-Rbxl.height = "100%"
-document.body.appendChild(Rbxl)
