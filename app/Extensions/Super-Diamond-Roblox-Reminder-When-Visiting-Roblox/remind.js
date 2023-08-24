b172d5cc517c6bc48dfc669bbea8a2ebb98586b6
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-08-24 20:12:35 +0300

    Create remind.js

---
+++ app/Extensions/Super-Diamond-Roblox-Reminder-When-Visiting-Roblox/remind.js
@@ -0,0 +1,4 @@
+if(confirm("Reminder: Would you like to go to Super Diamond Roblox")) {
+ location.replace("https://alexidians.github.io/Super-Diamond-Roblox/")
+ alert("We could not detect if you have Super Diamond Roblox Open it from your files or download it if you don't have it")
+}
